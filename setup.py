ae49b0b99a83ecec68737cb9b6bb79524dbe251f
ДМ. Па <qtckpuhdsa@gmail.com>
2021-01-09 12:09:09 +0300

    right sofmax

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cost2fitness", 
-    version="2.0.0",
+    version="2.0.1",
     author="Demetry Pascal",
     author_email="qtckpuhdsa@gmail.com",
     maintainer = ['Demetry Pascal'],
